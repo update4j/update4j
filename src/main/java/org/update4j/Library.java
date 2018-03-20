ba3ea199d13c293fe3b13795e7366b5ef1a15386
Mordechai Meisels <mordechai112@gmail.com>
2018-03-20 16:13:34 -0400

    Added license header

--- src/main/java/org/update4j/Library.java
+++ src/main/java/org/update4j/Library.java
@@ -1,19 +1,28 @@
+/*
+ * Copyright 2018 Mordechai Meisels
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.update4j;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.PrivateKey;
 import java.util.Base64;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.update4j.util.FileUtils;
 
 public class Library {
--- src/main/java/org/update4j/util/FileUtils.java
+++ src/main/java/org/update4j/util/FileUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Mordechai Meisels
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.update4j.util;
 
 import java.io.DataInputStream;
@@ -5,7 +20,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,17 +29,12 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.Signature;
 import java.security.SignatureException;
-import java.util.List;
-import java.util.ServiceLoader;
-import java.util.ServiceLoader.Provider;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.zip.Adler32;
 import java.util.zip.ZipFile;
 
 import org.update4j.OS;
-import org.update4j.service.Service;
 
 public class FileUtils {
 
