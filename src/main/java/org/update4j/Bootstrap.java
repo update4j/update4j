22b69ced5cd7ff4ed22acce5a532dc36c6774483
Mordechai Meisels <mordechai112@gmail.com>
2018-04-08 15:04:26 -0400

    Javadoc

--- src/main/java/org/update4j/Bootstrap.java
+++ src/main/java/org/update4j/Bootstrap.java
@@ -23,8 +23,54 @@ import org.update4j.service.Delegate;
 import org.update4j.service.Service;
 import org.update4j.util.StringUtils;
 
+/**
+ * This class consists of convenience methods and the module's main method
+ * to locate and start the bootstrap application in <a href=
+ * "https://github.com/update4j/update4j/wiki/Documentation#dealing-with-providers">Delegate
+ * Mode</a>.
+ * 
+ * @author Mordechai Meisels
+ *
+ */
 public class Bootstrap {
 
+	/**
+	 * The main method to start the bootstrap application in Delegate Mode from
+	 * command-line.
+	 * 
+	 * <p>
+	 * Here's how you should run the application:
+	 * 
+	 * <pre>
+	 * $ java --module-path . --module org.update4j
+	 * </pre>
+	 * 
+	 * Or in shorthand:
+	 * 
+	 * <pre>
+	 * $ java -p . -m org.update4j
+	 * </pre>
+	 * 
+	 * <p>
+	 * By default it will try to locate the highest versioned provider of
+	 * {@link Delegate}
+	 * (specified by {@link Service#version()}). You may override this behavior by
+	 * passing the delegate class name using the {@code --delegate} option:
+	 * 
+	 * <pre>
+	 * $ java --module-path . --module org.update4j --delegate=com.example.MyDelegate
+	 * </pre>
+	 * 
+	 * The class name should be the <i>Canonical Class Name</i> i.e. the String
+	 * returned
+	 * when calling {@link Class#getCanonicalName()}.
+	 * 
+	 * 
+	 * <p>
+	 * If the system cannot locate the passed class it will fall back to the default,
+	 * i.e. the highest version.
+	 * 
+	 */
 	public static void main(String[] args) throws Throwable {
 		String override = null;
 
@@ -40,10 +86,32 @@ public class Bootstrap {
 		start(override, List.of(args));
 	}
 
+	/**
+	 * Starts the bootstrap by locating the highest versioned provider of
+	 * {@link Delegate}
+	 * (specified by {@link Service#version()}).
+	 * 
+	 * @throws Throwable
+	 *             Any throwable thrown in the bootstrap.
+	 */
 	public static void start() throws Throwable {
 		start((String) null);
 	}
 
+	/**
+	 * Starts the bootstrap by locating the class between the list of registered
+	 * providers
+	 * of {@link Delegate}.
+	 * 
+	 * <p>If the system cannot locate any registered provider with the given name, the
+	 * highest versioned provider
+	 * (specified by {@link Service#version()}) will be used instead.
+	 * 
+	 * 
+	 * 
+	 * @throws Throwable
+	 *             Any throwable thrown in the bootstrap.
+	 */
 	public static void start(String override) throws Throwable {
 		start(override, List.of());
 	}
