3fe25b2972ab4d27aa8c2826d801556114c2aa35
Mordechai Meisels <mordechai112@gmail.com>
2018-09-03 15:27:07 -0400

    Moved version string to its own class

--- src/main/java/org/update4j/Bootstrap.java
+++ src/main/java/org/update4j/Bootstrap.java
@@ -35,11 +35,6 @@ import org.update4j.util.StringUtils;
 public class Bootstrap {
 
 	/**
-	 * The version of the current build of the framework.
-	 */
-	public static final String VERSION = "1.2.2";
-
-	/**
 	 * The main method to start the bootstrap application in Delegate Mode from
 	 * command-line.
 	 * 
