d09d9645d0a4e72e7459fa9b70dcebb7bbd10076
Mordechai Meisels <mordechai112@gmail.com>
2018-09-03 15:27:21 -0400

    License header

--- src/main/java/org/update4j/service/DefaultBootstrap.java
+++ src/main/java/org/update4j/service/DefaultBootstrap.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Mordechai Meisels
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.update4j.service;
 
 import java.io.IOException;
@@ -15,10 +30,10 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.update4j.Bootstrap;
 import org.update4j.Configuration;
 import org.update4j.SingleInstanceManager;
 import org.update4j.Update;
+import org.update4j.Update4j;
 
 public class DefaultBootstrap implements Delegate {
 
@@ -310,11 +325,11 @@ public class DefaultBootstrap implements Delegate {
 	private static void usage() {
 
 		System.err.println("To start in modulepath:\n\n" 
-						+ "\tjava -p update4j-" + Bootstrap.VERSION + ".jar -m org.update4j [commands...]\n"
+						+ "\tjava -p update4j-" + Update4j.version() + ".jar -m org.update4j [commands...]\n"
 						+ "\tjava -p . -m org.update4j [commands...]\n\n"
 						+ "To start in classpath:\n\n" 
-						+ "\tjava -jar update4j-" + Bootstrap.VERSION + ".jar [commands...]\n"
-						+ "\tjava -cp update4j-" + Bootstrap.VERSION + ".jar org.update4j.Bootstrap [commands...]\n"
+						+ "\tjava -jar update4j-" + Update4j.version() + ".jar [commands...]\n"
+						+ "\tjava -cp update4j-" + Update4j.version() + ".jar org.update4j.Bootstrap [commands...]\n"
 						+ "\tjava -cp * org.update4j.Bootstrap [commands...]\n\n" 
 						+ "Available commands:\n\n"
 						+ "\t--remote=[url] - The remote (or if using file:/// scheme - local) location of the\n"
