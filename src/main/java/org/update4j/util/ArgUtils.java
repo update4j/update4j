f470f5db2bf78dd330a20ade04eacdbe65039823
Mordechai Meisels <mordechai112@gmail.com>
2018-11-15 23:41:04 -0500

    License header. Corrected message for validateNoValue()

--- src/main/java/org/update4j/util/ArgUtils.java
+++ src/main/java/org/update4j/util/ArgUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Mordechai Meisels
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.update4j.util;
 
 import java.util.LinkedHashMap;
@@ -67,7 +82,7 @@ public class ArgUtils {
 
 	public static void validateNoValue(Map.Entry<String, String> e) {
 		if (e.getValue() != null) {
-			throw new IllegalArgumentException("Unknown command '" + e.getKey() + "'.");
+			throw new IllegalArgumentException("Unknown command '" + e.getValue() + "'.");
 		}
 	}
 
