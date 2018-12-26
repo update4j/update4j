bf571c909941ee07b089e1b99de06814f3b9763c
Mordechai Meisels <mordechai112@gmail.com>
2018-12-26 14:29:48 -0500

    Version bump

--- src/main/java/org/update4j/Bootstrap.java
+++ src/main/java/org/update4j/Bootstrap.java
@@ -37,7 +37,7 @@ public class Bootstrap {
 	/**
 	 * The version of the current build of the framework.
 	 */
-	public static final String VERSION = "1.3.3";
+	public static final String VERSION = "1.4.0";
 
 	/**
 	 * The main method to start the bootstrap application in Delegate Mode from
